3e7c751c3d36b680e1d755c50823dffd08a5130f
Shinan Liao <shinanliao1@gmail.com>
2021-06-17 14:13:24 -0700

    initial commit with popper

---
+++ src/views/Popper.vue
@@ -0,0 +1,24 @@
+<template>
+  <popper
+    trigger="clickToOpen"
+    :options="{
+      placement: 'top',
+      modifiers: { offset: { offset: '0,5px' } },
+    }"
+  >
+    <div class="popper">Cheese is delicious</div>
+
+    <button slot="reference">If you click on me you'll know something really important</button>
+  </popper>
+</template>
+
+<script>
+import Popper from "vue-popperjs";
+import "vue-popperjs/dist/vue-popper.css";
+
+export default {
+  components: {
+    popper: Popper,
+  },
+};
+</script>
